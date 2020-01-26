a621803fa026b2692b1a155d75172f7c1df0ef57
Austin Saflarske <auztin410@gmail.com>
2020-01-26 11:23:12 -0500

    random api test and adding into npm package json

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-var inquirer = require("inquirer");
 var request = require('request');
-var fs = require("fs");
+var fs = require('fs');
 
-var translationRequest = "This is a test translation!";
+var translationRequest = 'This is a test translation!';
 
-function translationAPI (){
-    request(`https://api.funtranslations.com/translate/yoda.json?text=${translationRequest}`, { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(res.body);
-});
+function translationAPI() {
+	request(
+		`https://api.funtranslations.com/translate/yoda.json?text=${translationRequest}`,
+		{ json: true },
+		(err, res, body) => {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(res.body);
+		}
+	);
 }
 
 // function textConvert (){
@@ -18,17 +23,31 @@ function translationAPI (){
 // }
 
 // textConvert();
-translationAPI();
+//translationAPI();
 
-var promptGo = true;
+function randomAPI() {
+	const axios = require('axios');
 
-if(promptGo === true) {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "This is a test prompt",
-            choices: ["A", "B", "C", "D"],
-            name: "options"
-        }
-    ])
+	axios
+		.post(`https://api.random.org/json-rpc/2/invoke`, {
+			jsonrpc: '2.0',
+			method: 'generateIntegers',
+			params: {
+				apiKey: '5620e5c6-c925-469b-93df-6d647d5dcde7',
+				n: 1,
+				min: 1,
+				max: 20,
+				replacement: true
+			},
+			id: 42
+		})
+		.then((res) => {
+			console.log(`statusCode: ${res.statusCode}`);
+			console.log(res.data.result.random.data);
+		})
+		.catch((error) => {
+			console.error(error);
+		});
 }
+
+randomAPI();
